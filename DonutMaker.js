ca517f946fbbd651b0b886ce942f1e8c10699ebb
TylerGarvey <redgarvey@gmail.com>
2022-11-04 21:01:11 -0400

    added modal and started styling

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,79 +1,88 @@
 
 export default class DonutMaker {
-    constructor() {
-      this.donutCount = 0;
-      this.clickValue = 1;
-      this.autoClickerCount = 0;
-      this.donutMultiplierCount = 0;
-      this.autoClickerCost = 5;
-      this.autoClickerRate = 1;
-      this.donutMultiplierCost = 10;
-    }
+  constructor() {
+    this.donutCount = 0;
+    this.clickValue = 1;
+    this.autoClickerCount = 0;
+    this.donutMultiplierCount = 0;
+    this.autoClickerCost = 100;
+    this.autoClickerRate = 1;
+    this.donutMultiplierCost = 10;
+    this.autoClickerHandles = [];
+  }
 
-    makeDonut(){
-      this.donutCount += this.clickValue;
+  killAutoClickers(){
+    for(const h of this.autoClickerHandles){
+      clearInterval(h)
     }
-    getClickValue(){
-      return this.clickValue;
-    }
+  }
 
-    getDonutCount() {
-      return Math.round(this.donutCount);
-    }
-    
+  makeDonut() {
+    this.donutCount += this.clickValue;
+  }
+  getClickValue() {
+    return this.clickValue;
+  }
 
-    getAutoClickerCount() {
-      return this.autoClickerCount
-    }
+  getDonutCount() {
+    return this.donutCount.toFixed(2);
+  }
 
-    getAutoClickerRate() {
-      return this.autoClickerRate;
-    }
-    // TODO may not need
-    getAutoClickerCost() {
-      return this.autoClickerCost;
-    }
-    
-    
- 
-    adjustAutoClickerCost() {
-      this.autoClickerCost == this.autoClickerCost * 1.1;
-    }
-    purchaseAutoClicker(){
-      if(this.donutCount >= this.autoClickerCost){
-        this.autoClickerCount ++;
-        this.donutCount -= this.autoClickerCost;
-        this.adjustAutoClickerCost;
-    } else if(this.donutCount < this.autoClickerCost){
+  getAutoClickerCount() {
+    return this.autoClickerCount
+  }
+
+  getAutoClickerRate() {
+    return this.autoClickerRate++;
+  }
+  
+  getAutoClickerCost() {
+    return this.autoClickerCost;
+  }
+
+  adjustAutoClickerCost() {
+    this.autoClickerCost += Math.round(this.autoClickerCost * 0.1);
+  }
+  purchaseAutoClicker() {
+    if (this.donutCount >= this.autoClickerCost) {
+      this.autoClickerCount++;
+      this.donutCount -= this.autoClickerCost;
+      this.adjustAutoClickerCost();
+    } else if (this.donutCount < this.autoClickerCost) {
       console.log("NOT ENOUGH DONUTS!")
     }
-    }
+  }
 
-    getDonutMultiplierCount() {
-      return this.donutMultiplierCount;
+  getDonutMultiplierCount() {
+    return this.donutMultiplierCount;
+  }
+
+  getDonutMultiplierCost() {
+    return this.donutMultiplierCost.toFixed(2);
+  }
+
+  adjustDonutMultiplierCost() {
+    this.donutMultiplierCost += (this.donutMultiplierCost * 0.1);
+  }
+
+  purchaseDonutMultiplier() {
+    if (this.donutCount >= this.donutMultiplierCost) {
+      this.donutMultiplierCount += 1;
+      this.donutCount -= this.donutMultiplierCost;
+      console.log('Multi', this.donutMultiplierCount);
+      console.log('multi$$$', this.donutMultiplierCost);
+      this.adjustDonutMultiplierCost();
+      this.adjustClickValue();
+    } else if (this.donutCount < this.donutMultiplierCost) {
+      console.log("NOT ENOUGH DONUTS!")
     }
-    getDonutMultiplierCost(){
-      return this.donutMultiplierCost;
-    }
-    adjustDonutMultiplierCost(){
-      this.donutMultiplierCost == this.donutMultiplierCost * 1.1;
-    }
-    purchaseDonutMultiplier(){
-      if(this.donutCount >= this.donutMultiplierCost){
-        this.donutMultiplierCount += 1;
-        this.donutCount -= this.donutMultiplierCost;
-        this.adjustDonutMultiplierCost;
-        this.adjustClickValue;
-      }else if(this.donutCount < this.autoClickerCost){
-        console.log("NOT ENOUGH DONUTS!")
-      }
-    }
-    adjustClickValue(){
-      this.clickValue == Math.pow(1.2, this.donutMultiplierCount);
-    }
-  
-  
+  }
+
+  adjustClickValue() {
+    this.clickValue = Math.pow(1.2, this.donutMultiplierCount);
+  }
+
+
 }
 
-  
-  
+
--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const DonutCount = document.getElementById("donutCount");
 const BuyAutoClicker = document.getElementById("buyAutoClicker");
 const AutoClickerCount = document.getElementById("autoClickerCount");
 const AutoClickerCost = document.getElementById("autoClickerCost");
-
-// may want to change this name 
+const MultiplierCount = document.getElementById("multiplierCount");
+const MultiplierCost = document.getElementById("multiplierCost");
+const BuyDonutMultiplier = document.getElementById("buyDonutMultiplier");
+const CurrentClickValue = document.getElementById("currentClickValue");
+const AboutGame = document.getElementById("aboutGame");
+const AboutCreator = document.getElementById("aboutCreator");
+const ResetButton = document.getElementById("resetButton")
+const AboutGameButton = document.getElementById("aboutGameButton");
+let isShowing = false;
 let dm = new DonutMaker();
 function showDisplay() {
   DonutCount.value = dm.getDonutCount()
   AutoClickerCount.value = dm.getAutoClickerCount();
   AutoClickerCost.value = dm.getAutoClickerCost();
+  MultiplierCount.value = dm.getDonutMultiplierCount();
+  MultiplierCost.value = dm.getDonutMultiplierCost();
+  CurrentClickValue.value = dm.getClickValue();
 };
 
 showDisplay();
@@ -22,27 +32,86 @@ MakeDonut.addEventListener("click", () => {
 }); 
 
 BuyAutoClicker.addEventListener("click", () => {
-  console.log("Buying auto clicker");
-  dm.buyAutoClicker();
+  dm.purchaseAutoClicker();
 
   let clickRate = dm.getAutoClickerRate();
   let timeInSec = 1000/clickRate;
   let autoClicking = setInterval(donutClickFactory, timeInSec);
-
   function donutClickFactory() {
-    console.log("auto click")
-    if( clickRate >= 0) {
-      console.log("click rate")
-      showDisplay();
-    } else {
-      ("maybe autoclick")
+    if( clickRate > 0) {
       dm.makeDonut();
+      dm.getAutoClickerCost()
       showDisplay();
-    }
+    } 
   }
-
+  dm.autoClickerHandles.push(autoClicking)
 });
 
+BuyDonutMultiplier.addEventListener("click", () => {
+  dm.purchaseDonutMultiplier();
+  showDisplay();
+})
+
+function getAboutGame() {
+  return `
+    <div class="expand-fred">
+      <p>Fred the Baker was a popular advertising character portrayed by actor Michael Vale in commercials for Dunkin' Donuts from 1981 to 1997. Michael Vale died on December 24, 2005 in New York City of complications from diabetes. He was 83 years old.</p>
+    </div>
+    <div class="expand-inspiration">
+      <p>Donut Maker draws heavily from 
+        <a href="https://orteil.dashnet.org/cookieclicker/" target="_blank">
+          COOKIE CLICKER!
+        </a>
+      </p>
+    </div>
+  `
+}
+
+function getAboutGameButton() {
+return `
+    <div class="dropdown">
+      <button id="aboutGame">About The Game</button>
+    </div>
+`
+};
+
+function getAboutCreator() {
+  return `
+  <div class="about-creator-content-container">
+   <div class="expand-contact">
+     <ul class="contact-info-list">
+       <li>Tyler.Garvey614@gmail.com</li>
+       <li>937-422-7859</li>
+      </ul>
+    </div>
+  </div>
+  `
+}
+
+AboutGameButton.addEventListener("click", (ev) => {
+    const modal = document.getElementById("aboutGameModal")
+    if (!isShowing) {
+      console.log("if ",isShowing)
+      modal.style.display = 'block'
+      isShowing = true;
+    } else {
+      console.log("else ",isShowing)
+      modal.style.display = 'none'
+      isShowing = false;
+    }
+});
+
+AboutCreator.addEventListener("click", () => {
+  AboutCreator.innerhtml = getAboutCreator();
+});
+
+ResetButton.addEventListener("click", () => {
+  dm.killAutoClickers();
+  dm = new DonutMaker();
+  showDisplay();
+});
+// let playSound = () => new Audio("src").play()
+// <button onclick="playSound()">Play</button>
 
 
 
